1f9097027466727b88afb12f2c0a679d661740e4
Scott Tutone <william.tutone@gmail.com>
2023-06-15 00:15:17 +1200

    Asking user to login or register

--- School-Lunch/School-Lunch/School-Lunch.cpp
+++ School-Lunch/School-Lunch/School-Lunch.cpp
@@ -2,14 +2,44 @@
 //Student names: Scott Tutone, 
 
 #include <iostream>
+#include <ctype.h>
 
 using namespace std;
+
+struct user {
+    
+    string username;
+    string password;
+    int age = 0;
+    bool admin;
+};
 
 int main()
 {
     cout << "Welcome to the school lunch system" << endl;
     cout << "----------------------------------" << endl;
 
+    char initial_input = '\n';
+
+    cout << endl << "Would you like to login (L/l) or register (R/r)? ";
+    cin >> initial_input;
+    tolower(initial_input);
+
+    while (initial_input != 'l' && initial_input != 'r') {
+        cin.clear();
+        cin.ignore();
+        cout << "Please enter in a valid input ";
+        cin >> initial_input;
+    }
+    /*
+    if (initial_input == ) {
+    
+    }
+    else if () {
+    
+    }
+    */
+    
     return 0;
 }
 
