f43c164a9f305925b547a20fd77b71a234a858f7
Scott Tutone <william.tutone@gmail.com>
2023-06-19 16:18:16 +1200

    registration of account been implemented, login functionality soon to follow

--- School-Lunch/School-Lunch/School-Lunch.cpp
+++ School-Lunch/School-Lunch/School-Lunch.cpp
@@ -56,61 +56,101 @@ struct beverage {                    // beverage structure declaration and const
 
 int main()                                                                             //welcome menu
 {
-    cout << "Welcome to the school lunch system" << endl;
-    cout << "----------------------------------" << endl;
+    bool run = true;                                                                   //THis will ensuer multiple iterations of the program can be run at the same time
+    while (run) {
+        
+        cout << "Welcome to the school lunch system" << endl;
+        cout << "----------------------------------" << endl;
 
-    cout << "1.Place an order" << endl;
-    cout << "2.View lunch Cart" << endl;
-    cout << "3.Login or Register" << "\n" << endl;
-    cout << "4.Exit" << "\n" << endl;
+        char initial_input = '\n';
 
-    char initial_input = '\n';
+        user entries[5];                                                                                 //Able to store entities so a person may logout and sign in as a different user
 
-    user entries[5];                                                                                 //Able to store entities so a person may logout and sign in as a different user
+        cout << endl << "To utulise this you program you must have an account" << endl;
+        cout << "Up to five users can log in at a time. " << endl;
+        cout << "Would you like to login (L/l) or register (R/r)? ";
 
-    cout << endl << "For this program up to five users may register and log in " << endl;
-    cout << "Would you like to login (L/l) or register (R/r)? ";
-    
-    cin >> initial_input;
-    
-    initial_input = tolower(initial_input);
-
-    while (initial_input != 'l' && initial_input != 'r') {
-        cin.clear();
-        cin.ignore();
-        cout << "Please enter in a valid input ";
         cin >> initial_input;
-    }
-    if (initial_input == 'l') {
-        
-    }
-    else if (initial_input == 'r') {                                                                            
-        
-        string name;
-        string password;
-        int age;
-        bool admin = false;
-        
-        cout << endl << "To create a new account please enter in the follwoing details " << endl;
-        cout << endl << "Enter in your username: ";
-        cin.ignore(1,'\n');                                                                                      //Since using the cin functions results in leftover of '\n' in the buffer the getline function reads it as done and skips past it 
-        getline(cin, name);                                                                                      //Therefore this ignore function will ignore the newline character allowing the user to input the data they need to
-        
-        cout << "Please enter in a strong password: ";
-        getline(cin, password);                                                                                  //getline function used as it also will include the spaces for user inputs
-        
-        cout << "Please enter in your age: ";
-        while (!(cin >> age) || age<0)
-        {
-            cout << "ERROR: a valid number must be entered: ";
+
+        initial_input = tolower(initial_input);
+
+        while (initial_input != 'l' && initial_input != 'r') {
             cin.clear();
-            cin.ignore(123, '\n');
+            cin.ignore();
+            cout << "Please enter in a valid input ";
+            cin >> initial_input;
+        }
+        if (initial_input == 'l') {
+            
+            if (entries[0].age == 0) {
+                cout << endl <<"There are no users currently registered in the program! " << endl;
+                cout << "Please create an account to get started" << endl << endl;
+                initial_input = 'r';
+            }
+
+        }
+        else if (initial_input == 'r') {
+
+            string name;
+            string password;
+            int age;
+            char occupation = '\n';
+            bool admin = false;
+
+            cout << endl << "To create a new account please enter in the following details " << endl;
+            cout << endl << "Enter in your username: ";
+            cin.ignore(1, '\n');                                                                                      //Since using the cin functions results in leftover of '\n' in the buffer the getline function reads it as done and skips past it 
+            getline(cin, name);                                                                                      //Therefore this ignore function will ignore the newline character allowing the user to input the data they need to
+
+            cout << "Please enter in a strong password: ";
+            getline(cin, password);                                                                                  //getline function used as it also will include the spaces for user inputs
+
+            cout << "Please enter in your age: ";
+                
+            while (!(cin >> age) || age < 0)                                                                          //input handler to catch unwanted inputs
+            {
+                cout << "ERROR: a valid number must be entered: ";
+                cin.clear();
+                cin.ignore(123, '\n');
+            }
+
+            cout << "Are you a student or a teacher at this institution (s/t): ";
+
+            cin >> occupation;
+
+            while (!cin >> occupation && occupation != 's' && occupation != 't') {
+
+                cout << "Please enter in a valid input specified within the brackets" << endl;
+                cin.clear();
+                cin.ignore();
+            }
+
+            if (occupation == 's') {
+                admin = false;
+            }
+            else if (occupation == 't') {
+                admin = true;
+            }
+
+            user entry1 = user(name, password, age, admin);                                                           //creating a new user defined variable.            
+            
+            int iteration = 0;
+
+            while (iteration < 5)                                                                                               //As the list is a static list we can use a while loop to iterate through the array and find an empty spot to store the information
+            {   
+                if (entries[iteration].age == 0) {
+                    entries[iteration] = entry1;
+                    break;
+                }
+                else {
+                    cout << "Sorry the maximum number of entries have been made please login or restart the program" << endl;
+                }
+            }
+
+            cout << "Account has been successfully created! PLease login and access our menu" << endl;
         }
 
-
     }
-   
-    
     return 0;
 }
 
